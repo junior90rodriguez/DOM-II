b8bd6a995511a29180b04411e81e7f8c4bd167f5
Junior <junior.arodriguez@icloud.com>
2019-04-13 17:12:54 -0500

    added click event listener and mouseover and mouseleave

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+const button = document.querySelectorAll('.btn');
+button.forEach(function(element){
+    element.addEventListener('dblclick', function(e){
+        console.log('Sign Me Up!')
+        e.target.style.background = 'red';
+    })
+})
+
+
+const input = document.querySelectorAll('p');
+input.forEach(function(element){
+    element.addEventListener('mouseover', function(e){
+        console.log('Moused over')
+        e.target.style.color = 'blue';
+    })
+    input.forEach(function(element){
+        element.addEventListener('mouseleave', function(e){
+            console.log('Moused leave')
+            e.target.style.color = '';
+        })
+    })
+        
+    })
+
+
+
